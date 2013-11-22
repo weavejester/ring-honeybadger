daaa08df107c7617d0fd3382b2e62b6c9c32472b
James Reeves <jreeves@weavejester.com>
2013-11-22 17:21:13 +0000

    Added server information to exception map

--- src/ring/middleware/honeybadger.clj
+++ src/ring/middleware/honeybadger.clj
@@ -1,10 +1,17 @@
 (ns ring.middleware.honeybadger
   (:require [clj-stacktrace.core :as st]
             [clj-http.client :as http]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [clojure.java.io :as io]))
 
 (def endpoint
   "https://api.honeybadger.io/v1/notices")
+
+(def hostname
+  (.getHostName (java.net.InetAddress/getLocalHost)))
+
+(def current-dir
+  (.getCanonicalPath (io/file ".")))
 
 (defn honeybadger-map [ex options]
   {:notifier {:name "Ring Honeybadger Middleware"
@@ -15,11 +22,16 @@
            :backtrace (for [t (:trace-elems ex)]
                         {:number (:line t)
                          :file   (:file t)
-                         :method (or (:method t) (:fn t))})}})
+                         :method (or (:method t) (:fn t))})}
+   :server {:project_root     {:path current-dir}
+            :environment_name (:env options "development")
+            :hostname         hostname}})
 
 (defn send-exception! [ex options]
   (http/post endpoint {:content-type :json
                        :accept :json
+                       :save-request? true
+                       :debug-body true
                        :headers {"X-API-Key" (:api-key options)}
                        :body (json/generate-string (honeybadger-map ex options))}))
 
